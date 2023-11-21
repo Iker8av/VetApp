b03446479d09d184c1874d851e9683479de84609
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-11-21 08:10:41 -0600

    App init

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import LogIn from './src/components/LogIn';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <LogIn />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
